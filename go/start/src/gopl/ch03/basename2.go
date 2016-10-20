9fd7502c027bd3a21fa1efa808b203a62288670d
Zhong Wei <zhongwei99@163.com>
2016-10-20 22:25:37 +0800

    source code for ch03 ch04 and part of ch05

---
+++ go/start/src/gopl/ch03/basename2.go
@@ -0,0 +1,8 @@
+func basename(s string) string {
+    slash := strings.LastIndex(s, "/")
+    s = s[slash+1:]
+    if dot := strings.LastIndex(s, "."); dot >= 0 {
+        s = s[:dot]
+    }
+    return s
+}
---
+++ go/start/src/gopl/ch04/github/search.go
@@ -0,0 +1,42 @@
+package github
+
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "net/url"
+    "strings"
+)
+
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+    q := url.QueryEscape(strings.Join(terms, " "))
+    resp, err := http.Get(IssuesURL + "?q=" + q)
+    if err != nil {
+        return nil, err
+    }
+
+    /*
+    req, err := http.NewRequest("GET", IssueURL+"?q="+q, nil)
+
+    if err != nil {
+        return nil, err
+    }
+
+    req.Header.Set("Accept", "application/vnd.github.v3.text-match+json")
+    resp, err := http.DefaultClient.Do(req)
+    */
+    if resp.StatusCode != http.StatusOK {
+        resp.Body.Close()
+        return nil, fmt.Errorf("search query failed: %s", resp.Status)
+    }
+
+    var result IssuesSearchResult
+    if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+        resp.Body.Close()
+        return nil, err
+    }
+
+    resp.Body.Close()
+    return &result, nil
+}
+
