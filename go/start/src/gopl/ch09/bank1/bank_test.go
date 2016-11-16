ecdd95ca354d141805d847fa7a081da4d89eaec5
Zhong Wei <zhongwei99@163.com>
2016-11-16 22:09:35 +0800

    ADD: ch09 source code

---
+++ go/start/src/gopl/ch09/bank1/bank_test.go
@@ -0,0 +1,31 @@
+package bank_test
+
+import (
+    "fmt"
+    "testing"
+
+    "gopl/ch09/bank1"
+)
+
+func TestBank(t *testing.T) {
+    done := make(chan struct{})
+
+    go func() {
+        bank.Deposit(200)
+        fmt.Println("=", bank.Balance())
+        done <- struct{}{}
+    }()
+
+    go func() {
+        bank.Deposit(100)
+        done <- struct{}{}
+    }()
+
+    <-done
+    <-done
+
+    if got, want := bank.Balance(), 300; got != want {
+        t.Errorf("Balance = %d, want %d", got, want)
+    }
+}
+
